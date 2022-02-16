c3aabf548c38e784810ac549f522a51db24381af
Bhavika Tibrewal <42600164+bhtibrewal@users.noreply.github.com>
2022-02-16 23:50:08 +0530

    Feat: wishlist page & cart page (#4)
    * Feat: wishlist page & cart page
    
    * fixes
    
    * minor fixes

--- index.js
+++ index.js
@@ -4,26 +4,22 @@ const signinSection = document.querySelector(".signin-section-overlay");
 
 const closeFunc = (e) => {
     e.parentElement.style.display = "none";
-    console.log("clicked")
 }
 const openLogin = () => {
     loginSection.style.display = "flex";
-    console.log("click")
 }
 const openSignup = () => {
     loginSection.style.display = "none";
     signinSection.style.display = "none";
     signupSection.style.display = "flex";
-    console.log("click")
 }
 const openSignin = () => {
     loginSection.style.display = "none";
     signupSection.style.display = "none";
     signinSection.style.display = "flex";
-    console.log("click")
 }
-let on = false;
-const openDropdown = (e) => {
-    on = !on;
-    e.nextElementSibling.style.display = on ? "flex" : "none";
-}
+// let on = false;
+// const openDropdown = (e) => {
+//     on = !on;
+//     e.nextElementSibling.style.display = on ? "flex" : "none";
+// }
